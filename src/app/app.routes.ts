749d6b553eca92abe885cd171166f169296abc9f
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-17 12:13:11 +0200

    added files and made alot of changes and nested out dashboard and users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AuthenticatorComponent } from './Pages/authenticator/authenticator.component';
+import { SidebarComponent } from './UI/Shared-UI/sidebar/sidebar.component';
+import { DashboardComponent } from './UI/Shared-UI/dashboard/dashboard.component';
+import { UserComponent } from './UI/Shared-UI/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: ' ', redirectTo:'login', pathMatch:'full'},
+{path: 'login', component: AuthenticatorComponent},
+
+{path: '', component: SidebarComponent,
+
+	children: [
+
+	{path: 'dashboard', component: DashboardComponent},
+	{path: 'users', component: UserComponent},
+
+],
+
+
+},
+
+
+];
+
