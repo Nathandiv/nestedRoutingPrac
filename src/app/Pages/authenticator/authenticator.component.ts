dafb89e81473eb98d0de82db63d88dbed166c274
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-17 16:01:55 +0200

    added function

--- src/app/Pages/authenticator/authenticator.component.ts
+++ src/app/Pages/authenticator/authenticator.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authenticator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './authenticator.component.html',
   styleUrl: './authenticator.component.css'
 })
 export class AuthenticatorComponent {
+
+ loginObj:any = {
+  username:'',
+  password:'',
+  };
+
+router = inject(Router);
+
+ onLogin(){
+if(this.loginObj.username === 'Admin' && this.loginObj.password === 123){
+  this.router.navigateByUrl('/dashboard')
+}
+else{
+  alert('wrong credentials');
+}
+ }
+
 
 }
